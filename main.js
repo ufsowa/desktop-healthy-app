598185c8dc96e921b41e428bebd6efeeab3345c4
Piotr Sowa <piotrsowa87@gmail.com>
2024-03-17 16:00:29 +0100

    [task] Init project

---
+++ main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+function main() {
+
+  // create new window
+  let mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    width: 800,
+    height: 600,
+  })
+
+  // enable dev tools
+  mainWindow.webContents.openDevTools();
+
+  // load app/index.html as the window content
+  mainWindow.loadFile(path.join('app', 'index.html'));
+}
+
+app.on('ready', main);
+
+app.on('window-all-closed', function () {
+  app.quit();
+});
